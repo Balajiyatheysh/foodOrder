1335053d667b0211b44fa999f546afe143ed5a56
balajiyatheysh <balajiyatheysh87@gmail.com>
2024-01-24 00:04:50 +0530

    configuring store

---
+++ src/utils/cartSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items:[]
+  },
+  reducers: {
+    addItems : (state, action) =>{
+      state.items.push(action.payload);
+    },
+    clearCart:(state)=>{
+      state.items.length = 0;
+    },
+    removeItem: (state)=>{
+      state.items.pop();
+    }
+  }
+});
+export const {addItems, clearCart, removeItem} = cartSlice.actions;
+export default cartSlice.reducer;
